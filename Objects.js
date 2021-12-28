44ed63bde2085571718b68ab04e3efcb29af69bf
saurabhraj042 <saurabhraj042@gmail.com>
2021-12-28 18:28:13 +0530

    add object manipulation concepts

--- Objects.js
+++ Objects.js
@@ -3,3 +3,18 @@ let user = {
     age: 30,
     "likes birds": true // multiword property name must be quoted
 };
+
+const owner = {
+    name: "Saurabh Raj",
+    age: 22,
+    email: "example@gmail.com"
+}
+console.log(owner)
+
+owner.age = 23 // Even if we use the const type with objects we can basically change the key-values information inside it.
+
+console.log(owner)
+
+delete owner.email
+
+console.log(owner)
