763f11453e432ec292a4ed7346c790048e48cca4
saurabhraj042 <saurabhraj042@gmail.com>
2021-12-28 12:27:06 +0530

    add variable file

---
+++ Variables.js
@@ -0,0 +1,13 @@
+/*
+Demonstration of Variables in JavaScript
+ */
+
+// Const are immutable so their value cannot be changed.
+const userName = 'Saurabh Raj'
+const userHometown = 'Bhagalpur'
+
+const USERDATEOFBIRTH = '22/10/1999'
+
+// Let and Var are mutable.
+let currentCityofUser = 'Ranchi'
+var currentPhoneNo = 123456789
